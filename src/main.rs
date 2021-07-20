d61dd9360bad21b234d4bb0a637b6a9747ea80fc
MohammadReza Rahmatzadeh <mr.rahmatzade371@gmail.com>
2021-07-20 12:46:37 +0430

    feat configure

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, guard};
+use actix_web::{web, App, HttpResponse, HttpServer};
 
+// this function could be located in a different module
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| HttpResponse::Ok().body("test")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+// this function could be located in a different module
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| HttpResponse::Ok().body("app")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(
-                web::scope("/")
-                    .guard(guard::Header("Host", "www.rust-lang.org"))
-                    .route("", web::to(|| HttpResponse::Ok().body("www"))),
-            )
-            .service(
-                web::scope("/")
-                    .guard(guard::Header("Host", "users.rust-lang.org"))
-                    .route("", web::to(|| HttpResponse::Ok().body("user"))),
-            )
-            .route("/", web::to(|| HttpResponse::Ok()))
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .route("/", web::get().to(|| HttpResponse::Ok().body("/")))
     })
     .bind("127.0.0.1:8080")?
     .run()
