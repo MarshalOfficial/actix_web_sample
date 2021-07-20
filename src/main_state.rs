a0958b422e5614ebbf839e83faa4fb7355c0dc8e
MohammadReza Rahmatzadeh <mr.rahmatzade371@gmail.com>
2021-07-20 12:23:02 +0430

    app with mutex state

---
+++ src/main_state.rs
@@ -0,0 +1,29 @@
+use actix_web::{get, web, App, HttpServer};
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+
+    format!("Hello {}!", app_name) // <- response with app_name
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .data(AppState {
+                app_name: String::from("Actix-web"),
+            })
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+//Any number of state types could be registered within the application.
