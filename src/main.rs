410972a4bf3b4cb317766891918c368e7b07ddef
MohammadReza Rahmatzadeh <mr.rahmatzade371@gmail.com>
2021-07-20 12:12:55 +0430

    app state

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    format!("Hello {}!", app_name) // <- response with app_name
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .data(AppState {
+                app_name: String::from("Actix-web"),
+            })
+            .service(index)
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
+
+//Any number of state types could be registered within the application.
